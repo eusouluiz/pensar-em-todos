c2a09db37a1487c2660c67025cff3dec85b5e241
Luiz G. Nascimento <luizgustavo.on@hotmail.com>
2021-01-17 20:49:37 -0300

    full project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function redirect_student() {
+    window.location.href = "./html/student.html";
+}
+
+function redirect_donation() {
+    window.location.href = "./html/donation.html";
+}
+
+function redirect_volunteer() {
+    window.location.href = "./html/volunteer.html";
+}
